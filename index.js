c5da23aebd9d03ebcfe2f8463b5a4d890b32dd06
Teresa Huynh <thuynh.developer@gmail.com>
2021-10-03 21:00:57 -0600

    adding index.js; users and run npm start to get a prompt to enter in a value and receive next closest palindrome

---
+++ index.js
@@ -0,0 +1,10 @@
+const { palindrome } = require('./helpers/palindrome');
+const prompt = require('prompt-sync')();
+
+const nbr = prompt('Enter a number: ');
+const closestPalindrome = palindrome(nbr);
+
+if (!closestPalindrome || closestPalindrome.includes('Error'))
+    console.log(`Looks like there was an issue: "${closestPalindrome || 'null returned'}"\n`);
+else 
+    console.log(`The next closest palindrome is ${palindrome(nbr)}\n`)
